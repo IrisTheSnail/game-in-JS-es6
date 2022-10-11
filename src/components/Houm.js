2bd6e76769285d2116d090fc8c9d384f8748d374
IrisTheSnail <salwa999adlouni@gmail.com>
2022-10-11 21:50:56 +0100

    still has to fix the aboutMe

--- src/components/Houm.js
+++ src/components/Houm.js
@@ -2,17 +2,15 @@ export default function Houm(){
     return(
         <div className="center">        
         <div className="mii"> 
-            <div className="mi">
+            <div className="mi" id="texte">
                 <h1 className="txt" id="txt1">Welcome to my website!</h1>
                 <div className="txt" id="txt2">
 
-                coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo 
-            coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo 
+                coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo 
             coo coo coo coo
             </div>
             </div>
-            <img className="mi cv" src="mi.png"
-                    alt="mi"></img>
+            <img className="mi cv" src="mi.png" alt="mi"></img>
         </div>
         </div>
     );
