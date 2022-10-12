81102724e078bd1015251b350eac0cc997dc5c41
IrisTheSnail <salwa999adlouni@gmail.com>
2022-10-12 23:29:19 +0100

    choose what you ll put in the page

--- src/components/Houm.js
+++ src/components/Houm.js
@@ -1,17 +1,33 @@
 export default function Houm(){
-    return(
-        <div className="center">        
-        <div className="mii"> 
-            <div className="mi" id="texte">
-                <h1 className="txt" id="txt1">Welcome to my website!</h1>
-                <div className="txt" id="txt2">
+    var w = window.innerWidth;
+    if(w >= 617){
+        return(
+            <div className="mii" id="coo">
+                <div className="mi" id="texte">
+                    <h1 className="txt" id="txt1">Welcome to my website!</h1>
+                    <div className="txt" id="txt2">
 
-                coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo 
-            coo coo coo coo
+                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
+                        coo coo coo coo
+                    </div>
+                </div>
+                <img className="mi cv" src="mi.png" alt="mi"></img>
             </div>
+        );
+        
+    }else { //2 : we re on a tablet or a cellphone
+        return(
+            <div className="mii2" id="coo2">
+                <img className="mi2 cv2" src="mi.png" alt="mi"></img>
+                <div className="mi2" id="texte2">
+                    <h1 className="txt2">Welcome to my website!</h1>
+                    <div className="txt2">
+
+                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
+                        coo coo coo coo
+                    </div>
+                </div>
             </div>
-            <img className="mi cv" src="mi.png" alt="mi"></img>
-        </div>
-        </div>
-    );
+        );
+    }
 }
