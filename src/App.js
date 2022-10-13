f9bcca2f8ff27bf1b2451a1c677097ac2e19f81f
IrisTheSnail <salwa999adlouni@gmail.com>
2022-10-13 23:51:25 +0100

    the about me part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './style/app.css';
 import './style/houm.css';
 import './style/header.css';
+import './style/aboutMe.css';
 
 import Header from './components/header';
 import Houm from './components/Houm';
+import About from './components/aboutMe';
 
 export default function App() {
   return (
     <>
       <Header></Header>
       <Houm></Houm>
+      <About></About>
     </>
   );
 }
---
+++ src/components/aboutMe.js
@@ -0,0 +1,14 @@
+
+export default function About(){
+    return(
+        <div className="txtabt"> 
+        <h1 id="path">About Me</h1>
+        <div> bla bla bla about my life (prepa, ensias, parascolaire...) 
+            coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
+            coo coo coo coo
+        </div>
+        <h2>hobbies</h2>
+        </div>
+    );
+}
+
