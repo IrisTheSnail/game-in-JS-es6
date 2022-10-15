41d025918e1d6b5ebdd49e273aa208f82a18364f
IrisTheSnail <salwa999adlouni@gmail.com>
2022-10-15 19:32:27 +0100

    made my own django font aawesome haha

--- src/components/skills.js
+++ src/components/skills.js
@@ -12,6 +12,40 @@ export default function Skills(){
                     <i className="fa fa-python fab"></i> <div className="bone"></div> 
                     <div className="ref">Python</div>
                 </div>
+
+                <div className="chri7a">
+                    <div className="fa">    
+                        <svg stroke="black" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M12.207 16.278C11.1241 17.343 9.63879 18 8 18C4.68629 18 2 15.3137 2 12C2 8.68629 4.68629 6 8 6C9.67492 6 11.1896 6.6863 12.278 7.79303L13.6923 6.37878C12.2418 4.91014 10.2272 4 8 4C3.58172 4 0 7.58172 0 12C0 16.4183 3.58172 20 8 20C10.1911 20 12.1764 19.1192 13.6212 17.6923L12.207 16.278Z" fill="currentColor"></path><path d="M15 9H13V11H11V13H13V15H15V13H17V11H15V9Z" fill="currentColor"></path><path d="M20 9H22V11H24V13H22V15H20V13H18V11H20V9Z" fill="currentColor"></path></svg>
+                    </div>
+                    <div className="bone"></div> 
+                    <div className="ref">C/C++</div>
+                </div>
+
+                <div className="chri7a">
+                    <i className="fa fa-brands fa-node-js"></i>
+                    <div className="bone"></div> 
+                    <div className="ref">NodeJS</div>
+                </div>
+
+                <div className="chri7a">
+                    <i className="fa fa-react fab"></i>
+                    <div className="bone"></div> 
+                    <div className="ref">ReactJS</div>
+                </div>
+
+                <div className="chri7a">
+                <div className="fa">
+                
+<svg width="25px" height="25px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
+<g>
+<rect fill="#092E20" x="0" y="0" width="256" height="256" rx="28"></rect>
+<path d="M186.377012,94.197636 L186.377012,160.424478 C186.377012,183.243286 184.707519,194.187745 179.699037,203.649406 C175.060358,212.741266 168.937684,218.490548 156.323731,224.798721 L129.794937,212.183571 C142.410087,206.247593 148.531564,201.05481 152.427049,193.074749 C156.509231,184.91278 157.808923,175.451119 157.808923,150.593015 L157.808923,94.197636 L186.377012,94.197636 Z M140.928486,50.0787207 L140.928486,182.316986 C126.272844,185.099476 115.512688,186.212472 103.826231,186.212472 C68.9487718,186.212472 50.7686431,170.445031 50.7686431,140.205054 C50.7686431,111.079269 70.0629644,92.1583404 99.9295492,92.1583404 C104.567032,92.1583404 108.091519,92.5281423 112.359199,93.6411381 L112.359199,50.0787207 L140.928486,50.0787207 Z M102.713236,115.160254 C88.243093,115.160254 79.8944275,124.065418 79.8944275,139.647359 C79.8944275,154.860696 87.8720944,163.208164 102.527736,163.208164 C105.680028,163.208164 108.278215,163.022665 112.359199,162.467364 L112.359199,116.643052 C109.020212,115.531253 106.237722,115.160254 102.713236,115.160254 Z M186.377012,50.2307105 L186.377012,79.5419941 L157.808923,79.5419941 L157.808923,50.2307105 L186.377012,50.2307105 Z" fill="#FFFFFF"></path>
+    </g>
+</svg>
+                </div>
+                    <div className="bone"></div> 
+                    <div className="ref">Django</div>
+                </div>
             </div>
         </>
     );
