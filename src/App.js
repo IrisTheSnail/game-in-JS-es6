22890ebced493735af4b5b2ed1443ee250e266e4
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-11 22:27:23 +0100

    hehe U should organise l component dyal <Projects>

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function App() {
       <About></About>
       <Skills></Skills>
       <Projects></Projects>
+      
     </>
   );
 }
--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,20 +5,21 @@ export default function About(){
         <h1 className="whoami">Who am I?</h1>
         <div className="balance">
             <div className="path"><div className="bolder">My path :</div> 
-                <div> prepa l3ibat
-                coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo 
+                <div> After my two years in preparatory classes (CPGE),
+                    I chose to pursue a Software Engineering degree in National School of Computer Science and Systems Analysis (ENSIAS) in Rabat, Morocco,
+as a child, I have always been interested with computers and informaion technology.
+I regularly learn new things in tech, as I made it a habit to work everyday on something, and yes,
+I do happen to have many projects laying around unfinished
+I tackled 
+As a result, I became fluent in classics like C/C++, Javascript and Python.
+I look forward to get my degree in 2024.
+
                 </div>
             </div>
             <div className="separator"></div>
             <div className="hobbies"><div className="bolder">Hobbies and interests :</div>
             coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-                        coo coo coo coo 
+
             </div>
         </div>
         </div>
