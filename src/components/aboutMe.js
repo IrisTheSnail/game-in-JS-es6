3780cb548e603b0228cd0417238e1bca54b2c769
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-14 23:49:57 +0100

    tmra - flexbox - github

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,21 +5,21 @@ export default function About(){
         <h1 className="whoami">Who am I?</h1>
         <div className="balance">
             <div className="path"><div className="bolder">My path :</div> 
-                <div> After my two years in preparatory classes (CPGE),
+                <div> After two years of intense study in preparatory classes (CPGE),
                     I chose to pursue a Software Engineering degree in National School of Computer Science and Systems Analysis (ENSIAS) in Rabat, Morocco,
-as a child, I have always been interested with computers and informaion technology.
-I regularly learn new things in tech, as I made it a habit to work everyday on something, and yes,
-I do happen to have many projects laying around unfinished
-I tackled 
-As a result, I became fluent in classics like C/C++, Javascript and Python.
+as a child, I have always been interested with computers and IT.
+I regularly learn new things in tech, as I made it a habit to work everyday to sharpen my skills.
+I am fluent in classics like <i className="bb">C/C++</i>, <i className="bb">Javascript</i> and <i className="bb">Python</i>.
 I look forward to get my degree in 2024.
-
                 </div>
             </div>
             <div className="separator"></div>
             <div className="hobbies"><div className="bolder">Hobbies and interests :</div>
-            coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo coo
-
+            Apart from coding, there are a lot of activities I enjoy, such as :<br/>
+            <i className="fa-compass far"></i>  -  travelling<br/>
+            <i className="fa-gamepad fas"></i> - videogames<br/>
+            <i className="fa-chess fas"></i>  -  chess<br/>
+            
             </div>
         </div>
         </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,15 @@
+
+
 export default function Projects(){
     return(
-        <></>
+        <>
+            <div className="skillset">Projects</div>
+            <div classname="skillflex">
+                <div className="tmra">
+                    <div className="">  </div>
+                    
+                </div>
+            </div>
+        </>
     );
 }
