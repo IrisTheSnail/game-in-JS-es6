0a47667dda754db0ff0fa78a66c92396e2c92b01
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-15 00:56:33 +0100

    tmra - flexbox - github

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,11 +3,12 @@
 export default function Projects(){
     return(
         <>
-            <div className="skillset">Projects</div>
-            <div classname="skillflex">
+            <div className="skillset projicts">Projects</div>
+            <div className="skillflex ccc">
                 <div className="tmra">
-                    <div className="">  </div>
+                    <div className="proj-title"> Puzzle </div>
                     
+                    <div className="fortran"> This is a puzzle game  </div>
                 </div>
             </div>
         </>
