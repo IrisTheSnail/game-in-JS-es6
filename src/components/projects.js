87e944d36077fe23b1fedbbc12ff4278e075f6d3
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-18 20:19:13 +0100

    production ecrite

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,7 +8,8 @@ export default function Projects(){
                 <div className="tmra">
                     <div className="proj-title"> Puzzle </div>
                     
-                    <div className="fortran"> This is a puzzle game  </div>
+                    <div className="fortran"> A digital form of a puzzle with 15 nails and a triangular board that looks like a crossover between chess and checkers, it has a variety of solutions but the question is : how many times you're gonna have
+                    to lose and reset the board before finding one of the solutions?  </div>
                 </div>
             </div>
         </>
